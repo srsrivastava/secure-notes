e0e0d6c94607c2000660cdf2cb8d4eeb2b21fedb
Sudhanshu <srsrivastava1926@gmail.com>
2025-08-27 19:10:16 +0530

    Set up structure of Notes

---
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.secure.notes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) ->
+                requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
