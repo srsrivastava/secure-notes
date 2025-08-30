1f0260c9821cf3bf328d5c683f8d7eef22efd7b6
Sudhanshu <srsrivastava1926@gmail.com>
2025-08-30 15:07:26 +0530

    Implement Role Based Authorization

---
+++ src/main/java/com/secure/notes/controllers/AdminController.java
@@ -0,0 +1,44 @@
+package com.secure.notes.controllers;
+
+import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.models.User;
+import com.secure.notes.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+public class AdminController {
+
+    @Autowired
+    UserService userService;
+
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(),
+                HttpStatus.OK);
+    }
+
+    @PutMapping("/update-role")
+    public ResponseEntity<String> updateUserRole(@RequestParam Long userId, 
+                                                 @RequestParam String roleName) {
+        userService.updateUserRole(userId, roleName);
+        return ResponseEntity.ok("User role updated");
+    }
+
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserById(id),
+                HttpStatus.OK);
+    }
+
+
+}
